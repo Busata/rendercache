3eab061ce9e1199f830a517bcf35f19e87dbd86f
busata <driesdesmet@gmail.com>
2022-04-28 00:05:45 +0200

    Fixes

--- src/main/java/org/veevi/rendercache/api/ImageService.java
+++ src/main/java/org/veevi/rendercache/api/ImageService.java
@@ -1,5 +1,10 @@
 package org.veevi.rendercache.api;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.MetadataException;
+import com.drew.metadata.exif.ExifIFD0Directory;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.apache.tika.Tika;
@@ -11,6 +16,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 @Component
@@ -24,8 +30,8 @@ public class ImageService {
             final var data = loadImage(imageUrl);
 
             final var preferredHeight = (int) Math.ceil(width * data.ratio());
-            return data.updateImage(scaleImage(data.image(), preferredHeight, width));
-        } catch (IOException e) {
+            return data.updateImage(scaleImage(data.image(), preferredHeight, width, data.rotation()));
+        } catch (Exception e) {
             log.error("Could not scale image {} to width {}", imageUrl, width, e);
         }
         return null;
@@ -38,31 +44,55 @@ public class ImageService {
 
             final var preferredWidth = (int) Math.ceil(height * data.ratio());
 
-            return data.updateImage(scaleImage(data.image(), height, preferredWidth));
-        } catch (IOException e) {
+            return data.updateImage(scaleImage(data.image(), height, preferredWidth, data.rotation()));
+        } catch (Exception e) {
             log.error("Could not scale image {} to height {}", imageUrl, height, e);
         }
         return null;
     }
 
-    private BufferedImage scaleImage(BufferedImage data, int height, int preferredWidth) {
+    private BufferedImage scaleImage(BufferedImage data, int height, int preferredWidth, int rotation) {
         BufferedImage bufferedImage = new BufferedImage(preferredWidth, height, data.getType());
+        log.info("Rotation: {}", rotation);
         Graphics2D graphics = bufferedImage.createGraphics();
         graphics.drawImage(data, 0, 0, preferredWidth, height, null);
+        graphics.rotate(Math.toRadians(rotation), preferredWidth / 2f, height / 2f);
         graphics.dispose();
         return bufferedImage;
     }
 
 
-    public ImageData loadImage(String imageUrl) throws IOException {
+    public ImageData loadImage(String imageUrl) throws IOException, ImageProcessingException, MetadataException {
         final var imageStream = new URL(imageUrl).openStream();
+
+        int orientation = getOrientation(new URL(imageUrl).openStream());
+
         final var imageBytes = IOUtils.toByteArray(imageStream);
 
         final var format = MediaType.parse(tika.detect(imageBytes));
         final var image = ImageIO.read(new ByteArrayInputStream(imageBytes));
 
 
-        return new ImageData(format, image);
+        return new ImageData(format, image, orientation);
+    }
+
+    private int getOrientation(InputStream imageStream) throws ImageProcessingException, IOException, MetadataException {
+        Metadata metadata = ImageMetadataReader.readMetadata(imageStream);
+        ExifIFD0Directory exifIFD0 = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
+        int orientation = exifIFD0.getInt(ExifIFD0Directory.TAG_ORIENTATION);
+
+        return switch (orientation) {
+            case 1 -> // [Exif IFD0] Orientation - Top, left side (Horizontal / normal)
+                    0;
+            case 6 -> // [Exif IFD0] Orientation - Right side, top (Rotate 90 CW)
+                    90;
+            case 3 -> // [Exif IFD0] Orientation - Bottom, right side (Rotate 180)
+                    180;
+            case 8 -> // [Exif IFD0] Orientation - Left side, bottom (Rotate 270 CW)
+                    270;
+            default -> 0;
+        };
+
     }
 
 
